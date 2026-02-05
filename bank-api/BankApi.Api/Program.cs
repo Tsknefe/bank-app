31192a16bf79837db07a27962ed1d260d84488b5
tsknefe <taskiranefe5@gmail.com>
2026-02-05 11:20:52 +0300

    feat(#2): setup PostgreSQL and EF Core with migrations

--- bank-api/BankApi.Api/Program.cs
+++ bank-api/BankApi.Api/Program.cs
@@ -1,11 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using BankApi.Infrastructure;
+using BankApi.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<BankaDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<BankaDbContext>();
+
+await db.Database.MigrateAsync();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -14,28 +23,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
---
+++ bank-api/BankApi.Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApi.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string IdentıtyNumber { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+
+        public ICollection<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
